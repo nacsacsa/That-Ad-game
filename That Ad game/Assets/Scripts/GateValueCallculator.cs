0ca32544710961035a6edf9fa44b99a692dda635
Márton <kolozsi.ocsi@gmail.com>
2025-07-05 15:13:37 +0200

    add obsticle working text and color change

---
+++ That Ad game/Assets/Scripts/GateValueCallculator.cs
@@ -0,0 +1,70 @@
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+public class GateValueCallculator : MonoBehaviour
+{
+    private char[] operations = { '+', '/', '*' };
+    private int number;
+    private int numberMin = -5;
+    private int numberMax = 5;
+
+    private char operation;
+    private string gateLabel;
+
+    private Renderer rend;
+    private TextMeshPro textMesh ;
+
+    void Start()
+    {
+        Transform labelTransform = transform.Find("Label");
+        textMesh = labelTransform.GetComponent<TextMeshPro>();
+        rend = GetComponent<Renderer>();
+
+        operation = operations[Random.Range(0, operations.Length)];
+        switch (operation)
+        {
+            case '+':
+                number = Random.Range(numberMin, numberMax + 1);
+                gateLabel = number.ToString();
+                break;
+            default:
+                number = Random.Range(1, numberMax + 1);
+                gateLabel = operation.ToString() + number.ToString();
+                break;
+        }
+
+        switch (operation)
+        {
+            case '/':
+                SetColor(Color.red);
+                break;
+            case '*':
+                SetColor(Color.blue);
+                break;
+            default:
+                if (number < 0)
+                {
+                    SetColor(Color.red);
+                    break;
+                }
+                else if (number > 0)
+                {
+                    SetColor(Color.blue);
+                    break;
+                }
+                SetColor(Color.gray);
+                    break;
+        }
+
+        textMesh.text = gateLabel;
+    }
+
+    void SetColor(Color color)
+    {
+        if (rend != null)
+        {
+            rend.material.color = color;
+        }
+    }
+}
--- That Ad game/Assets/Scripts/Logic.cs
+++ That Ad game/Assets/Scripts/Logic.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class Logic : MonoBehaviour
 {
     public int Lives = 1;
-    public GameObject players;
-    public GameObject obsticles;
+    public GameObject player;
+    public GameObject obsticlesPrefab;
+    public GameObject playerPrefab;
+
+    private float timer = 0f;
+    public float spawnInterval = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +19,19 @@ public class Logic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            SpawnObstacle();
+            timer = 0f;
+        }
+    }
+
+    private void SpawnObstacle()
+    {
+        Vector3 spawnPosition = new Vector3(400f, 0.5f, -4.89094f);
+        Instantiate(obsticlesPrefab, spawnPosition, Quaternion.identity);
     }
 
     private bool GameOver()
